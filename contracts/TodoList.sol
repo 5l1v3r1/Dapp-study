d91027d7c4c1050ab9f69c64547a4b0a563a55e5
mr-m0nst3r <songxinlei@gmail.com>
2019-03-29 17:36:54 +0800

    2. List tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,27 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
 	uint public taskCount = 0;
+
+	// model the task
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	// mapping to access the storage
+	// todoList.tasks() access the items
+	mapping(uint => Task) public tasks; // using "public" so SC will provide a function for free
+
+	// put Task struct into mapping
+	function createTask(string memory _content) public {
+		taskCount ++;
+		//create Task item
+		tasks[taskCount] = Task(taskCount, _content, false);
+	}
+
+	//add some init tasks using contructor
+	constructor() public {
+		createTask("Init task using constructor");
+	}
 }
